2dc8b1b4e914634397b7e1692c0f5b88dc945dbc
Eric <leucome@hotmail.com>
2019-09-24 16:17:42 -0400

    Merging test

--- init.lua
+++ init.lua
@@ -11,8 +11,8 @@
 
 ]]--
 
-local wp = minetest.get_worldpath() .. "/luscious"
-minetest.mkdir(wp)
+gt = {}
+tablesize = 0
 
 local mgp = minetest.get_mapgen_params()
 local chunksize = 16 * mgp.chunksize
@@ -28,25 +28,34 @@ local function cmpy(p2, y)
 		return math.max(1, p2)
 	end
 end
+
+local function chose_color(pos)
+	-- Calculate color from biome heat and humidity.
+	local biome_data = minetest.get_biome_data(pos)
+	local hum1 = biome_data.humidity
+	local temp1 = biome_data.heat 
+	local vh = math.floor(math.min(math.max(math.floor(hum1), 0), 100) / 6.6)
+	local vt = math.floor(math.min(math.max(math.floor(temp1), 0), 100) / 6.6)
+	vt = vt - math.max(math.min(vt, math.floor(pos.y / 16)), 0)
+	return vh * 16 + vt
+end
+
 local function on_construct(pos)
 	-- get chunk from pos
 	local v = vector.apply(pos, function(a) return math.floor((a - 48) / chunksize) end)
 	local o = vector.subtract(pos, vector.apply(v, function(a) return (a * chunksize) + 48 end))
 	local l = o.z * (chunksize) + o.x
 	local p = minetest.hash_node_position(v)
-
-	local f = io.open(wp .. "/" .. string.format("%d", p), "r")
-	if not f then
-		minetest.log("error", "unable to find map for " .. string.format("%d", p))
-		return
-	end
-
-	local z = f:read("*a")
-	f:close()
-	local map = minetest.decompress(z)
-
+	local map = gt[p]
+	local mv = 0
 	local node = minetest.get_node(pos)
-	node.param2 = cmpy(string.byte(map, l + 1), pos.y)
+	if map == nil then
+		mv = chose_color(pos) 
+	else
+		mv = cmpy(string.byte(map, l + 1), pos.y)
+	end
+	if mv < 2 then mv = 2 end
+	node.param2 = mv
 	minetest.swap_node(pos, node)
 end
 
@@ -61,6 +70,71 @@ minetest.override_item("default:dirt_with_grass", {
 	place_param2 = 136,
 	on_construct = on_construct,
 })
+
+minetest.override_item("default:desert_sand", {
+	paramtype2 = "color",
+	drawtype = "color",
+	palette_index = 136,
+	color = "#c0825dff",
+	palette = "luscious_desert_sand_palette.png",
+	tiles = {"luscious_desert_sand.png", "luscious_desert_sand.png", "luscious_desert_sand.png"},
+	place_param2 = 136,
+	drop  = "default:desert_sand", -- Prevent the creation of a new item for each color.
+	on_construct = on_construct,
+})
+
+minetest.override_item("default:sand", {
+	paramtype2 = "color",
+	drawtype = "color",
+	palette_index = 136,
+	color = "#f3dec3ff",
+	palette = "luscious_default_sand_palette.png",
+	tiles = {"luscious_default_sand.png", "luscious_default_sand.png", "luscious_default_sand.png"},
+	place_param2 = 136,
+	drop  = "default:sand",
+	on_construct = on_construct,
+})
+
+--------------------------------------Ethereal Add On------------------------------------------------
+if minetest.get_modpath("ethereal") then
+minetest.override_item("ethereal:prairie_dirt", {
+	paramtype2 = "color",
+	drawtype = "color",
+	palette_index = 136,
+	color = "#7dea1eff",
+	palette = "luscious_grass_palette.png",
+	tiles = {"luscious_ethereal_prairie_grass.png", {name = "default_dirt.png", color = "white"}, "luscious_ethereal_prairie_grass.png"},
+	overlay_tiles = {"", "", {name = "luscious_dirt_overlay.png", color = "white"}},
+	place_param2 = 136,
+	drop  = "ethereal:prairie_dirt",
+	on_construct = on_construct,
+})
+
+minetest.override_item("ethereal:bamboo_dirt", {
+	paramtype2 = "color",
+	drawtype = "color",
+	palette_index = 136,
+	color = "#7dea1eff",
+	palette = "luscious_grass_palette.png",
+	tiles = {"luscious_ethereal_grass_bamboo_top.png", {name = "default_dirt.png", color = "white"}, "luscious_ethereal_grass_bamboo_top.png"},
+	overlay_tiles = {"", "", {name = "luscious_dirt_overlay.png", color = "white"}},
+	place_param2 = 136,
+	drop  = "ethereal:bamboo_dirt",
+	on_construct = on_construct,
+})
+
+minetest.override_item("ethereal:dry_dirt", {
+	paramtype2 = "color",
+	drawtype = "color",
+	palette_index = 136,
+	color = "#B79370ff",
+	palette = "luscious_ethereal_dry_dirt_palette.png",
+	tiles = {"luscious_ethereal_dry_dirt.png", {name = "default_dirt.png", color = "white"}, "luscious_ethereal_dry_dirt.png"},
+	place_param2 = 136,
+	drop  = "ethereal:dry_dirt",
+	on_construct = on_construct,
+})
+end
 
 for _, v in pairs({
 	"default:leaves",
@@ -80,6 +154,7 @@ for _, v in pairs({
 		color = "#8afb94ff",
 		tiles = {tile},
 		place_param2 = 136,
+		visual_scale = 1.5,
 		on_construct = on_construct,
 	})
 end
@@ -105,6 +180,7 @@ for _, v in pairs({
 		color = "#7dea1eff",
 		tiles = {tile},
 		place_param2 = 136,
+		visual_scale = 2,
 		on_construct = on_construct,
 	})
 end
@@ -114,6 +190,11 @@ end
 
 -- content ids
 local cn = {
+	["default:desert_sand"] = 1,
+	["default:sand"] = 1,
+	["ethereal:bamboo_dirt"] = 1,
+	["ethereal:dry_dirt"] = 1,
+	["ethereal:prairie_dirt"] = 1,
 	["default:dirt_with_grass"] = 1,
 	["default:grass_1"] = 1,
 	["default:grass_2"] = 1,
@@ -146,12 +227,14 @@ minetest.register_on_generated(function(minp, maxp, blockseed)
 		h2 = math.floor(math.min(math.max(math.floor(h2), 0), 100) / 6.6)
 		map = map .. string.char(h1 + (h2 * 16))
 	end
-	local p = string.format("%d", minetest.hash_node_position(v))
-
-	local f = assert(io.open(wp .. "/" .. p, "w"), wp .. "/" .. p)
-	f:write(minetest.compress(map))
-	f:close()
-
+	local p = minetest.hash_node_position(v)
+	if tablesize > 75 then
+		gt = {}
+		collectgarbage()
+		tablesize = 0
+	end
+	tablesize = tablesize + 1
+	gt[p] = map
 	local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
 	local area = VoxelArea:new{MinEdge = emin, MaxEdge = emax}
 	local data = vm:get_data()
@@ -163,6 +246,7 @@ minetest.register_on_generated(function(minp, maxp, blockseed)
 			local vv = (x - minp.x) + ((z - minp.z) * chunksize)
 			if cs[data[vi]] then
 				local mv = cmpy(string.byte(map, vv + 1), y)
+				if mv < 2 then mv = 2 end
 				p2data[vi] = mv
 			end
 			vi = vi + 1
